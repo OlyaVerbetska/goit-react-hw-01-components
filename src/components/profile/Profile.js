6a859dc65bfef6684197dc2f42c8a0b7aa267a03
Olga Verbetska <verbetska150@gmail.com>
2021-03-21 15:52:09 +0200

    clear commit

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,32 +1,32 @@
 import React from "react";
 
-const Profile = (props) => {
+const Profile = ({avatar, name, tag, location, stats}) => {
   return (
     <div class="profile">
       <div class="description">
         <img
-          src={props.avatar}
+          src={avatar}
           alt="Аватар пользователя"
           class="avatar"
         // Delete next string after studying styles React (module2)
           width="300px"
         />
-        <p class="name">{props.name}</p>
-        <p class="tag">@{props.tag}</p>
-        <p class="location">{props.location}</p>
+        <p class="name">{name}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
       </div>
       <ul class="stats">
         <li>
           <span class="label">Followers</span>
-          <span class="quantity">{props.stats.followers}</span>
+          <span class="quantity">{stats.followers}</span>
         </li>
         <li>
           <span class="label">Views</span>
-          <span class="quantity">{props.stats.views}</span>
+          <span class="quantity">{stats.views}</span>
         </li>
         <li>
           <span class="label">Likes</span>
-          <span class="quantity">{props.stats.likes}</span>
+          <span class="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
